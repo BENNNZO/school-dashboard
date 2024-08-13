04c79512e4dc67f2766d22be941c9f1f385dd6fe
Benjamin Phillips <benjamin.phillips.webdev@gmail.com>
2024-08-12 17:37:14 -0700

    finishing touches

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,24 +5,21 @@ export default function Home() {
 
 	return (
 		<main className="bg-stone-950 h-screen w-screen flex flex-col">
-			{/* <ul className="bg-green-700 text-white absolute top-5 left-5 rounded-md p-2">
-				<li className="uppercase font-bold text-center mb-5">Todo List</li>
-				<li>Quick Links</li>
-				<li>Todo List For Each Class</li>
-			</ul> */}
 			<h1 className="text-white text-center font-bold text-5xl py-5 tracking-wide border-b border-white/20">GMC Dashboard</h1>
 			<div className="flex flex-row h-full justify-around gap-px bg-white/20">
 				<ClassSection title={"Algebra"}>
-					<QuickLink title={"Moodle"} link="#" />
-					<QuickLink title={"MyLab"} link="#" />
+					<QuickLink title={"Moodle"} link="https://gmc.mrooms3.net/course/view.php?id=81166" />
+					<QuickLink title={"MyLab"} link="https://mylabmastering.pearson.com/?courseId=12966713#/" />
+					{/* <QuickLink title={"MyLab1"} link="https://socket.pearsoned.com/uiservice/optstatus/#/optstatus" /> */}
+					{/* <QuickLink title={"MyLab2"} link="https://tpi.bb.pearsoncmg.com/tpi/lti/basic/pearson/ext/" /> */}
 				</ClassSection>
 				
 				<ClassSection title={"Leadership"}>
-					<QuickLink title={"Moodle"} link="#" />
+					<QuickLink title={"Moodle"} link="https://gmc.mrooms3.net/course/view.php?id=82330" />
 				</ClassSection>
 
 				<ClassSection title={"English"}>
-					<QuickLink title={"Moodle"} link="#" />
+					<QuickLink title={"Moodle"} link="https://gmc.mrooms3.net/course/view.php?id=81173"/>
 				</ClassSection>
 			</div>
 		</main>
--- src/components/QuickLink.jsx
+++ src/components/QuickLink.jsx
@@ -3,7 +3,7 @@ export default function QuickLink(props) {
 
     return (
         <button className="bg-white/10 text-white rounded-sm hover:bg-white/20 duration-100">
-            <a href={link} className="p-2">
+            <a href={link} target="_blank" className="p-2">
                 <p className="text-lg">{title}</p>
             </a>
         </button>
