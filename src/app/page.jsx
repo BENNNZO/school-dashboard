8834258b410f10ff207054e92e10323b969f1743
Benjamin Phillips <benjamin.phillips.webdev@gmail.com>
2024-08-12 17:49:26 -0700

    added a max-w

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,23 +4,25 @@ import ClassSection from "@/components/ClassSection";
 export default function Home() {
 
 	return (
-		<main className="bg-stone-950 h-screen w-screen flex flex-col">
-			<h1 className="text-white text-center font-bold text-5xl py-5 tracking-wide border-b border-white/20">GMC Dashboard</h1>
-			<div className="flex flex-row h-full justify-around gap-px bg-white/20">
-				<ClassSection title={"Algebra"}>
-					<QuickLink title={"Moodle"} link="https://gmc.mrooms3.net/course/view.php?id=81166" />
-					<QuickLink title={"MyLab"} link="https://mylabmastering.pearson.com/?courseId=12966713#/" />
-					{/* <QuickLink title={"MyLab1"} link="https://socket.pearsoned.com/uiservice/optstatus/#/optstatus" /> */}
-					{/* <QuickLink title={"MyLab2"} link="https://tpi.bb.pearsoncmg.com/tpi/lti/basic/pearson/ext/" /> */}
-				</ClassSection>
-				
-				<ClassSection title={"Leadership"}>
-					<QuickLink title={"Moodle"} link="https://gmc.mrooms3.net/course/view.php?id=82330" />
-				</ClassSection>
+		<main className="bg-stone-950 h-screen w-screen">
+			<div className="max-w-[1500px] flex flex-col h-full border-x border-white/20 mx-auto">
+				<h1 className="text-white text-center font-bold text-5xl py-5 tracking-wide border-b border-white/20">GMC Dashboard</h1>
+				<div className="flex flex-row h-full justify-around gap-px bg-white/20">
+					<ClassSection title={"Algebra"}>
+						<QuickLink title={"Moodle"} link="https://gmc.mrooms3.net/course/view.php?id=81166" />
+						<QuickLink title={"MyLab"} link="https://mylabmastering.pearson.com/?courseId=12966713#/" />
+						{/* <QuickLink title={"MyLab1"} link="https://socket.pearsoned.com/uiservice/optstatus/#/optstatus" /> */}
+						{/* <QuickLink title={"MyLab2"} link="https://tpi.bb.pearsoncmg.com/tpi/lti/basic/pearson/ext/" /> */}
+					</ClassSection>
+					
+					<ClassSection title={"Leadership"}>
+						<QuickLink title={"Moodle"} link="https://gmc.mrooms3.net/course/view.php?id=82330" />
+					</ClassSection>
 
-				<ClassSection title={"English"}>
-					<QuickLink title={"Moodle"} link="https://gmc.mrooms3.net/course/view.php?id=81173"/>
-				</ClassSection>
+					<ClassSection title={"English"}>
+						<QuickLink title={"Moodle"} link="https://gmc.mrooms3.net/course/view.php?id=81173"/>
+					</ClassSection>
+				</div>
 			</div>
 		</main>
 	);
--- src/components/QuickLink.jsx
+++ src/components/QuickLink.jsx
@@ -1,10 +1,15 @@
+import Image from "next/image"
+
 export default function QuickLink(props) {
     const { title, link } = props
 
     return (
-        <button className="bg-white/10 text-white rounded-sm hover:bg-white/20 duration-100">
-            <a href={link} target="_blank" className="p-2">
-                <p className="text-lg">{title}</p>
+        <button className="bg-white/10 text-white rounded-sm hover:bg-white/20 duration-100 group py-2">
+            <a href={link} target="_blank" className="p-2 grid place-items-center">
+                <div className="relative h-8 w-full overflow-hidden">
+                    <p className="text-lg absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 group-hover:translate-y-12 duration-200">{title}</p>
+                    <p className="text-lg absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-12 group-hover:-translate-y-1/2 duration-200">{title}</p>
+                </div>
             </a>
         </button>
     )
