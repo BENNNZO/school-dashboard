d0c643abd1e1bfa79b54335543e5c31ca270cc01
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-01-07 10:22:55 -0500

    updated classes Y1 Q3

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,19 +11,19 @@ export default function Home() {
 				</h1>
 				<Utils />
 				<div className="flex flex-row h-full justify-around gap-px bg-neutral-800/50">
-					<ClassSection title={"Algebra"}>
-						<QuickLink title={"Moodle"} link="https://gmc.mrooms3.net/course/view.php?id=83664" />
-						<QuickLink title={"Syllabus"} link="https://gmc.mrooms3.net/mod/book/view.php?id=7328786&chapterid=478041" />
+					<ClassSection title={"Pre-Calculus"}>
+						<QuickLink title={"Moodle"} link="https://gmc.mrooms3.net/course/view.php?id=86105" />
+						<QuickLink title={"Syllabus"} link="https://gmc.mrooms3.net/mod/book/view.php?id=7552148&chapterid=509163" />
 					</ClassSection>
 
-					<ClassSection title={"American History"}>
-						<QuickLink title={"Moodle"} link="https://gmc.mrooms3.net/course/view.php?id=83635" />
-						<QuickLink title={"Syllabus"} link="https://gmc.mrooms3.net/mod/book/view.php?id=7324549&chapterid=477531" />
+					<ClassSection title={"American Government"}>
+						<QuickLink title={"Moodle"} link="https://gmc.mrooms3.net/course/view.php?id=86139" />
+						<QuickLink title={"Syllabus"} link="https://gmc.mrooms3.net/mod/book/view.php?id=7556846&chapterid=509787" />
 					</ClassSection>
 
-					<ClassSection title={"English II"}>
-						<QuickLink title={"Moodle"} link="https://gmc.mrooms3.net/course/view.php?id=83608" />
-						<QuickLink title={"Syllabus"} link="https://gmc.mrooms3.net/mod/book/view.php?id=7320550&chapterid=476966" />
+					<ClassSection title={"English III"}>
+						{/* <QuickLink title={"Moodle"} link="https://gmc.mrooms3.net/course/view.php?id=83608" /> */}
+						{/* <QuickLink title={"Syllabus"} link="https://gmc.mrooms3.net/mod/book/view.php?id=7320550&chapterid=476966" /> */}
 					</ClassSection>
 				</div>
 			</div>
@@ -31,19 +31,34 @@ export default function Home() {
 	)
 }
 
+// YEAR 1 | QUARTER 2 -----------------------------------------------------------------------------------
+
+// Algebra
+// Moodle: https://gmc.mrooms3.net/course/view.php?id=83664
+// Syllabus: https://gmc.mrooms3.net/mod/book/view.php?id=7328786&chapterid=478041
+
+// American History
+// Moodle: https://gmc.mrooms3.net/course/view.php?id=83635
+// Syllabus: https://gmc.mrooms3.net/mod/book/view.php?id=7324549&chapterid=477531
+
+// English II
+// Moodle: https://gmc.mrooms3.net/course/view.php?id=83608
+// Syllabus: https://gmc.mrooms3.net/mod/book/view.php?id=7320550&chapterid=476966
+
+// ------------------------------------------------------------------------------------------------------
 // YEAR 1 | QUARTER 1 -----------------------------------------------------------------------------------
 
 // Algebra
-// Moodle: https://gmc.mrooms3.net/course/view.php?id=81166" />
-// Syllabus: https://gmc.mrooms3.net/mod/book/view.php?id=7046287&chapterid=439071" />
-// MyLab: https://mylabmastering.pearson.com/?courseId=12966713#/" />
+// Moodle: https://gmc.mrooms3.net/course/view.php?id=81166
+// Syllabus: https://gmc.mrooms3.net/mod/book/view.php?id=7046287&chapterid=439071
+// MyLab: https://mylabmastering.pearson.com/?courseId=12966713#/
 
 // Leadership
-// Moodle: https://gmc.mrooms3.net/course/view.php?id=82330" />
-// Syllabus: https://gmc.mrooms3.net/mod/book/view.php?id=7228575&chapterid=469135" />
+// Moodle: https://gmc.mrooms3.net/course/view.php?id=82330
+// Syllabus: https://gmc.mrooms3.net/mod/book/view.php?id=7228575&chapterid=469135
 
 // English
-// Moodle: https://gmc.mrooms3.net/course/view.php?id=81173"/>
-// Syllabus: https://gmc.mrooms3.net/mod/book/view.php?id=7048062&chapterid=439233"/>
+// Moodle: https://gmc.mrooms3.net/course/view.php?id=81173
+// Syllabus: https://gmc.mrooms3.net/mod/book/view.php?id=7048062&chapterid=439233
 
 // ------------------------------------------------------------------------------------------------------
